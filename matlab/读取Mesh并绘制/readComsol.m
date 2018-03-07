1facbfee5fca999753762e138921738d26041a3a
YuchenYang1997 <yuchenyangyyc@outlook.com>
2018-03-06 19:18:27 -0800

    2nd goal completed.
    Signed-off-by: YuchenYang1997 <yuchenyangyyc@outlook.com>

--- matlab/读取Mesh并绘制/readComsol.m
+++ matlab/读取Mesh并绘制/readComsol.m
@@ -1,4 +1,4 @@
-function [xy,tri,DM] = readComsol(fname)
+function [xy,TR,DM] = readComsol(fname)
 %扫描文件获得每个单元的节点信息，以及每个域和其单元之间的关系
 fp = fopen(fname, 'r');%打开文件
 for i=1:18
@@ -50,8 +50,8 @@ end
 tri_nd_ele=fscanf(fp,'%d # number of nodes per element\n',[1,1]);
 tri_ele=fscanf(fp,'%d # number of elements\n',[1,1]);
 fgets(fp);
-tri=fscanf(fp,'%d %d %d \n',[3,tri_ele]);
-tri=tri'+1;
+TR=fscanf(fp,'%d %d %d \n',[3,tri_ele]);
+TR=TR'+1;
 
 
 tri_ind=fscanf(fp,'%d # number of geometric entity indices\n',[1,1]);
